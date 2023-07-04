7aa2eb1b19a8bc3d351abe29d3df6929043d7eef
pipez3 <138540716+pipez3@users.noreply.github.com>
2023-07-04 17:36:27 -0400

    Add files via upload

---
+++ script nuevo.py
@@ -0,0 +1,88 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: C:/Users/amanda/Desktop/ppp.py ===================
+Ciudad de Origen: santiago
+Ciudad de Destino: puerto mont
+Traceback (most recent call last):
+  File "C:/Users/amanda/Desktop/ppp.py", line 31, in <module>
+    distancia, duracion_segundos = calcular_distancia(ciudad_origen, ciudad_destino)
+  File "C:/Users/amanda/Desktop/ppp.py", line 9, in calcular_distancia
+    distancia = data['rows'][0]['elements'][0]['distance']['value'] / 1000  # Convertir a kilómetros
+IndexError: list index out of range
+
+============================== RESTART: C:/Users/amanda/Desktop/ppp.py ==============================
+Ciudad de Origen: peñalolen
+Ciudad de Destino: cerrillo
+Traceback (most recent call last):
+  File "C:/Users/amanda/Desktop/ppp.py", line 31, in <module>
+    distancia, duracion_segundos = calcular_distancia(ciudad_origen, ciudad_destino)
+  File "C:/Users/amanda/Desktop/ppp.py", line 9, in calcular_distancia
+    distancia = data['rows'][0]['elements'][0]['distance']['value'] / 1000  # Convertir a kilómetros
+IndexError: list index out of range
+
+============================== RESTART: C:/Users/amanda/Desktop/ppp.py ==============================
+Ciudad de Origen: santiago
+Ciudad de Destino: reñaca
+Traceback (most recent call last):
+  File "C:/Users/amanda/Desktop/ppp.py", line 31, in <module>
+    distancia, duracion_segundos = calcular_distancia(ciudad_origen, ciudad_destino)
+  File "C:/Users/amanda/Desktop/ppp.py", line 9, in calcular_distancia
+    distancia = data['rows'][0]['elements'][0]['distance']['value'] / 1000  # Convertir a kilómetros
+IndexError: list index out of range
+
+============================== RESTART: C:/Users/amanda/Desktop/ppp.py ==============================
+Ciudad de Origen: Santiago Centro, Santiago
+Ciudad de Destino: Museo Marítimo Nacional
+Traceback (most recent call last):
+  File "C:/Users/amanda/Desktop/ppp.py", line 31, in <module>
+    distancia, duracion_segundos = calcular_distancia(ciudad_origen, ciudad_destino)
+  File "C:/Users/amanda/Desktop/ppp.py", line 9, in calcular_distancia
+    distancia = data['rows'][1]['elements'][2]['distance']['value'] / 1000  # Convertir a kilómetros
+IndexError: list index out of range
+
+============================== RESTART: C:/Users/amanda/Desktop/ppp.py ==============================
+Ciudad de Origen: santiago
+Ciudad de Destino: buenos aires
+Traceback (most recent call last):
+  File "C:/Users/amanda/Desktop/ppp.py", line 31, in <module>
+    distancia, duracion_segundos = calcular_distancia(ciudad_origen, ciudad_destino)
+  File "C:/Users/amanda/Desktop/ppp.py", line 9, in calcular_distancia
+    distancia = data['rows'][0]['elements'][0]['distance']['value'] / 1000  # Convertir a kilómetros
+IndexError: list index out of range
+>>> 
+============================== RESTART: C:/Users/amanda/Desktop/ppp.py ==============================
+>>> 
+============================== RESTART: C:/Users/amanda/Desktop/ppp.py ==============================
+>>> 
+============================== RESTART: C:/Users/amanda/Desktop/ppp.py ==============================
+Ciudad de Origen: santiago
+Ciudad de Destino: buenos aires
+Traceback (most recent call last):
+  File "C:/Users/amanda/Desktop/ppp.py", line 31, in <module>
+    distancia, duracion_segundos = calcular_distancia(ciudad_origen, ciudad_destino)
+  File "C:/Users/amanda/Desktop/ppp.py", line 9, in calcular_distancia
+    distancia = data['rows'][0]['elements'][0]['distance']['value'] / 5000  # Convertir a kilómetros
+IndexError: list index out of range
+>>> 
+============================== RESTART: C:/Users/amanda/Desktop/ppp.py ==============================
+Ciudad de Origen: santiago
+Ciudad de Destino: buenosaires
+Traceback (most recent call last):
+  File "C:/Users/amanda/Desktop/ppp.py", line 31, in <module>
+    distancia, duracion_segundos = calcular_distancia(ciudad_origen, ciudad_destino)
+  File "C:/Users/amanda/Desktop/ppp.py", line 9, in calcular_distancia
+    distancia = data['rows'][0]['elements'][0]['distance']['value'] / 5000  # Convertir a kilómetros
+IndexError: list index out of range
+>>> 
+============================== RESTART: C:/Users/amanda/Desktop/ppp.py ==============================
+Ciudad de Origen: santiago
+Ciudad de Destino: buenos aires
+Traceback (most recent call last):
+  File "C:/Users/amanda/Desktop/ppp.py", line 31, in <module>
+    distancia, duracion_segundos = calcular_distancia(ciudad_origen, ciudad_destino)
+  File "C:/Users/amanda/Desktop/ppp.py", line 9, in calcular_distancia
+    distancia = data['rows'][0]['elements'][0]['distance']['value'] / 1000  # Convertir a kilómetros
+IndexError: list index out of range
+>>> 
+========================== RESTART: C:/Users/amanda/Desktop/script nuevo.py =========================
